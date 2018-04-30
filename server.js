92f63e10403b82cda4cfb2ce542182fbed76948d
christoph <chrisleitner@web.de>
2018-04-30 12:46:22 +0200

    Refactoring Server und Service

---
+++ server.js
@@ -0,0 +1,13 @@
+let express = require("express");
+let app = express();
+let port = 8081;
+
+app.use(express.static('./'));
+
+app.route("/*").get(function (req, res) {
+    res.redirect('/');
+});
+
+app.listen(port, function(){
+   console.info("http://localhost:" + port);
+});
